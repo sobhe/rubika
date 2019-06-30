8906cccbfe3a35691b70629a63204e9b7decb9c0
Alireza Nourian <az.nourian@gmail.com>
2019-06-30 18:03:22 +0430

    dump chart data to json file

--- dist/main.js
+++ dist/main.js
@@ -208,7 +208,7 @@ var renderPiechart = require('./piechart');
 (function ($) {
 	"use strict"; // Start of use strict
 
-	var queryUrl = 'https://app.jadoobi.com/sobhe/api/queries/12148/results.json?api_key=GLWHkzs5Tj18m79Hn1JpHRbaCI8vDOkUMsfqq8aX';
+	var queryUrl = '/js/days.json';
 	var data = null;
 
 	Chart.defaults.global.defaultFontFamily = "Shabnam";
--- js/main.js
+++ js/main.js
@@ -3,20 +3,20 @@ var renderBarchart = require('./barchart');
 var renderPiechart = require('./piechart');
 // var Chart = require('chartjs');
 
-(function ($) {
-	"use strict"; // Start of use strict
+(function($) {
+    "use strict"; // Start of use strict
 
-	var queryUrl = 'https://app.jadoobi.com/sobhe/api/queries/12148/results.json?api_key=GLWHkzs5Tj18m79Hn1JpHRbaCI8vDOkUMsfqq8aX';
-	var data = null;
+    var queryUrl = '/js/days.json';
+    var data = null;
 
-	Chart.defaults.global.defaultFontFamily = "Shabnam";
+    Chart.defaults.global.defaultFontFamily = "Shabnam";
 
-	$.get(queryUrl, function(queryResult){
-		data = utils.prepareRows(queryResult.query_result.data.rows);
-		renderBarchart(data);
-		renderPiechart(data);
-	});
-	
+    $.get(queryUrl, function(queryResult) {
+        data = utils.prepareRows(queryResult.query_result.data.rows);
+        renderBarchart(data);
+        renderPiechart(data);
+    });
+
 })(jQuery);
 
 require('./app');
